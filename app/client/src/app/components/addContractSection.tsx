a72fe9526ce9e64db609ab92967f253862e1953b
Måns Elfving <MansElfving@MacBook-Pro-7.local>
2023-10-06 15:50:11 +0200

    mobile

--- app/client/src/app/components/addContractSection.tsx
+++ app/client/src/app/components/addContractSection.tsx
@@ -13,6 +13,7 @@ import { useWallet } from '@solana/wallet-adapter-react';
 
 import styles from '../styles/addcontract.module.scss'
 import Link from 'next/link';
+import Image from 'next/image'
 
 import { useSearchParams, useRouter } from 'next/navigation'
 
@@ -32,6 +33,8 @@ export default function AddContractSection(){
     const [inputtedOwner, setInputtedOwner] = useState("");
 
     const [inputtedPassword, setInputtedPassword] = useState("");
+
+    const[menuActive, setMenuActive] = useState(false)
 
     const [accounts, setAccounts] = useState([]);
 
@@ -67,11 +70,48 @@ export default function AddContractSection(){
 
     return(
         <main className={styles.main}>
+            <div className={menuActive ? styles.menuDropDownActive : styles.menuDropDownInactive}>
+                <div className={styles.menuContainer}>
+                    <div className={styles.menuTop}>
+                        <Image
+                        src="/images/logov2.png"
+                        width={50}
+                        height={50}
+                        alt="Picture of the author"
+                        />
+
+                        <button className={styles.menuClose} onClick={() => setMenuActive(!menuActive)}>
+                        Close
+                        </button>
+                    </div>
+                    <div className={styles.menuMiddle}>
+                        <Link href="/dashboard" className={styles.navbarContent}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M20 19v-8.5a1 1 0 0 0-.4-.8l-7-5.25a1 1 0 0 0-1.2 0l-7 5.25a1 1 0 0 0-.4.8V19a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1zM8 13v3m4-6v6m4-1v1"/></svg>
+                            <p>Home</p>
+                        </Link>
+                        <Link href="/dashboard/contracts" className={styles.navbarContent}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M19 20H5c-1.6 0-2-1.333-2-2V6a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v6m2 8c-.667 0-2-.4-2-2v-6m2 8c1.6 0 2-1.333 2-2v-6h-4"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16h6m-1-6a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/></svg>
+                            <p>Contracts</p>
+                        </Link>
+                        <Link href="/dashboard/newcontract" className={styles.navbarContent}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M19 16v3m0 0v3m0-3h3m-3 0h-3m4-10v-.172a2 2 0 0 0-.586-1.414l-3.828-3.828A2 2 0 0 0 14.172 3H14m6 6h-4a2 2 0 0 1-2-2V3m6 6v3m-6-9H6a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h6"/></svg>
+                            <p>New Contract</p>
+                        </Link>
+                    </div>
+                    <div className={styles.menuBtm}>
+                        <Link className={styles.menuLoginBtn} href="/dashboard/newcontract"><p>+ Add Contract</p></Link>
+                    </div>
+                </div>
+                <div className={styles.menuOverlay}>
+
+                </div>
+            </div>
             <section id="searchbar" className={styles.searchbar}>
+                <button className={styles.lgHidden} onClick={() => setMenuActive(!menuActive)}>Menu</button>
                 <Link href="/dashboard" className={styles.searchbarInput}>
                     <p>Cancel</p>
                 </Link>
-                <div>{wallet?.publicKey.toString()}</div>
+                <div>{wallet?.publicKey.toString().substring(0, 4) + "..." + wallet?.publicKey.toString().substring(wallet?.publicKey.toString().length - 4)}</div>
             </section>
             <div id="hello" className={styles.helloContainer}>
                 <h1>Create New Contract</h1>
